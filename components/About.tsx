91c7beb6ebb09e05d70755d7dbe081f79abe628c
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-09-29 03:05:26 +0100

    created About section

---
+++ components/About.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import profilePic2 from '../public/images/charles2.jpg';
+
+type Props = {};
+
+function About({}: Props) {
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+
+      <motion.div
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        whileInView={{ opacity: 1, x: 0 }} // image slides in only when in view
+        viewport={{ once: true }}
+        className="relative -mb-20 mt-20  md:mt-0 xl:mt-60 sm:-mb-10 md:mb-0 flex-shrink-0 w-36 h-36 sm:w-48 sm:h-48 md:w-64 md:h-95 xl:w-[450px] xl:h-[550px]"
+      >
+        <Image
+          src={profilePic2}
+          alt="Profile picture of Charles Ukasoanya"
+          layout="fill"
+          className="rounded-full object-cover md:rounded-lg"
+        />
+      </motion.div>
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-2xl lg:text-4xl font-semibold">
+          Here is a <span className="myHighlight">little</span> background
+        </h4>
+        <p className="hidden md:block text-base">
+          I'm Charles Ukasoanya, a passionate{' '}
+          <span className="myHighlight">frontend developer</span>. My biggest
+          motivation comes from the thought that with just some key strokes on
+          the keyboard of my computer I can do amazing things, well in reality
+          thats a lot of keystrokes. I enjoy creating fast, clean, and
+          maintainable web applications, ready to scale from few users to
+          thousands. My past work experience as a{' '}
+          <span className="myHighlight">Crew Trainer</span> in the ever fast
+          pace McDonalds fast food chain ensures that I can deliver a seamless
+          and engaging experience to your users. Background in{' '}
+          <span className="myHighlight">
+            Information and Communication Technology
+          </span>{' '}
+          and ability as a self taught devloper (frontend-heavy and sufficient
+          back-end proficiency), allow me to create everything from small
+          business websites to custom web applications. I am{' '}
+          <span className="myHighlight">an avid podcast listener</span>
+          where I stay up to date with fresh and upcoming technologies in
+          fullstack software development. It is an exciting time to be a
+          developer and I look forward to the opportunities ahead and the
+          products that have yet to be built.
+        </p>
+        <p className="md:hidden text-base">
+          I'm Charles Ukasoanya, a passionate{' '}
+          <span className="myHighlight">frontend developer</span>. My biggest
+          motivation comes from the thought that with just some key strokes on
+          the keyboard of my computer I can do amazing things, well in reality
+          thats a lot of keystrokes. I enjoy creating fast, clean, and
+          maintainable web applications, ready to scale from few users to
+          thousands. I am{' '}
+          <span className="myHighlight">an avid podcast listener </span>
+          where I stay up to date with fresh and upcoming technologies in
+          fullstack software development. It is an exciting time to be a
+          developer and I look forward to the opportunities ahead and the
+          products that have yet to be built.
+        </p>
+      </div>
+    </motion.div>
+  );
+}
+
+export default About;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -37,7 +37,7 @@ function Hero({}: Props) {
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
           software engineer
         </h2>
-        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+        <h1 className="text-lg md:text-3xl lg:text-6xl font-semibold px-10">
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="#f7AB0A" />
         </h1>
