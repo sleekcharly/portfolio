cca00241c3b73e2df6c9441a4a18ef55d74b75fb
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-10-07 04:47:40 +0100

    fixed glitch bug from framer motion in chromium based browsers

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -30,7 +30,7 @@ function Skill({ directionLeft, skill }: Props) {
       </motion.div>
 
       {/* for skills title on hover*/}
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-16 h-16 md:w-24 md:h-24 xl:w-28 xl:h-28 rounded-full z-0">
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-16 h-16 md:w-24 md:h-24 xl:w-28 xl:h-28 rounded-full will-change-transform z-0">
         <div className="flex items-center justify-center h-full">
           <p className="text-xs md:text-lg font-bold text-black opacity-100">
             {skill?.title}
