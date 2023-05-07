79d7911558c4086f64022dde5c447f1b7f579b82
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2023-05-07 16:50:06 +0100

    Carried out folders sanitization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,3 @@
-// import '../styles/globals.css';
-// import type { AppProps } from 'next/app';
-// import { Toaster } from 'react-hot-toast';
-
-// function MyApp({ Component, pageProps }: AppProps) {
-//   return (
-//     <>
-//       <Toaster />
-//       <Component {...pageProps} />
-//     </>
-//   );
-// }
-
-// export default MyApp;
-
 import React from 'react';
 import '../styles/globals.css';
 import { Montserrat } from 'next/font/google';
