9891ff37e7805772f2672a9e97d558da1a83a5b1
Charles <c_ukasoanya@yahoo.com>
2023-12-27 11:05:39 +0100

    Updated resume

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home() {
 
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/01Charles_Ukasoanya.pdf"
+                  href="/01Ukasoanya-Charles.pdf"
                   target={`_blank`}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-white hover:dark:border-light md:p-2 md:px-4 md:text-base"
                   download={true}
